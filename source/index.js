580736fe3bebabd6205018c81bf4028c022d2c8c
InstantlyMoist <kyllian007@gmail.com>
2020-02-14 13:57:07 +0100

    added verification code handler

--- source/index.js
+++ source/index.js
@@ -1,4 +1,4 @@
-const { IgApiClient } = require('instagram-private-api');
+const { IgApiClient, IgCheckpointError } = require('instagram-private-api');
 const { readFile } = require('fs');
 const { promisify } = require('util');
 const { sample } = require('lodash');
@@ -10,7 +10,8 @@ const caption = require('./post/caption.js');
 const request = require('request');
 const fs = require('fs');
 const Jimp = require("jimp");
-
+const Bluebird = require('bluebird');
+const inquirer = require('inquirer');
 const credentials = require('./credentials.json');
 
 async function uploadPost() {
@@ -25,17 +26,29 @@ async function uploadPost() {
     const ig = new IgApiClient();
     ig.state.generateDevice(credentials.instagram.username);
     await ig.qe.syncLoginExperiments();
-    const loggedInUser = await ig.account.login(credentials.instagram.username, credentials.instagram.password);
-    const path = "./memes/meme.jpg";
-    console.log('uploading..');
-    const publishResult = await ig.publish.photo({
-      file: await readFileAsync(path),
-      caption: await caption.getCaption(),
-    });
+    Bluebird.try(async () => {
+      const auth = await ig.account.login(credentials.instagram.username, credentials.instagram.password);
+      console.log(auth);
+      const path = "./memes/meme.jpg";
+      console.log('uploading..');
+      const publishResult = await ig.publish.photo({
+        file: await readFileAsync(path),
+        caption: await caption.getCaption(),
+      });
+    }).catch(IgCheckpointError, async () => {
+      await ig.challenge.auto(true);
+      const { code } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'code',
+          message: 'Enter code',
+        },
+      ]);
+      console.log(await ig.challenge.sendSecurityCode(code));
+    }).catch(e => console.log('Could not resolve checkpoint:', e, e.stack));
+    
   });
 };
-
-uploadPost();
 
 async function downloadImageFromUrl(url, callback) {
   console.log("downloading");
@@ -61,9 +74,9 @@ async function convertMeme() {
   });
 }
 
-schedule.scheduleJob('* 0 * * *', () => {
-  let delay = (random.int(0, 10) * 1000);
-  console.log('De post wordt geplaatst om: ' + delay);
-  setTimeout(delay, uploadPost());
+schedule.scheduleJob('*/5 * * * *', () => {
+  let delay = (random.int(0, 2) * 1000);
+  console.log('De post wordt geplaatst over: ' + delay);
+  setTimeout(uploadPost, delay);
 });
 
